723d9947ae30e54b380d4c9028990b8fe1f9ac89
erikaledster <erikaledster@gmail.com>
2026-09-24 13:37:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='hq1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="rugby-diary-v2";
+const ASSETS=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
