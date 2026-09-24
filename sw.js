d021a69a4f928931e1032359abfee216b6ca36e4
erikaledster <erikaledster@gmail.com>
2026-09-24 13:41:32 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,23 @@
-const CACHE="rugby-diary-v2";
-const ASSETS=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="rugby-diary-v21";
+const ASSETS=["./manifest.json"];
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener("fetch",e=>{
+  if(e.request.mode==="navigate"){
+    e.respondWith(fetch(e.request).then(r=>{
+      const copy=r.clone(); caches.open(CACHE).then(c=>c.put("./index.html",copy)); return r;
+    }).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match(e.request)));
+});
