36523f53ed5b768d7e564c4bb8f14b76b4a6427d
erikaledster <erikaledster@gmail.com>
2026-09-24 13:50:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,5 @@
-const CACHE="rugby-diary-v21";
-const ASSETS=["./manifest.json"];
-self.addEventListener("install",e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener("activate",e=>{
-  e.waitUntil(Promise.all([
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
-    self.clients.claim()
-  ]));
-});
-self.addEventListener("fetch",e=>{
-  if(e.request.mode==="navigate"){
-    e.respondWith(fetch(e.request).then(r=>{
-      const copy=r.clone(); caches.open(CACHE).then(c=>c.put("./index.html",copy)); return r;
-    }).catch(()=>caches.match("./index.html")));
-    return;
-  }
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match(e.request)));
-});
+const CACHE='preseason-diary-v6';
+const ASSETS=['./','./index.html','./manifest.json','./icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
